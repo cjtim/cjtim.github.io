ba45932354248c04babc5ca5786c0a6b3d3ff62f
Jinna Chodchoy <47479090+cjtim@users.noreply.github.com>
2023-05-20 22:13:17 +0700

    add dark mode

---
+++ src/utils/theme.ts
@@ -0,0 +1,7 @@
+export const isDark = (): boolean =>
+    localStorage?.theme === 'dark' ??
+    (window?.matchMedia("(prefers-color-scheme: dark)").matches) ?? false;
+
+export const getTheme = (): 'light' | 'dark' => (
+    isDark() ? 'dark' : 'light'
+)
