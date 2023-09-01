a7cda53bfe802e936ba68a47206bc9d019fb4761
cjtim <47479090+cjtim@users.noreply.github.com>
2023-09-01 19:20:53 -0400

    add svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,15 +3,17 @@ import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), svelte()],
   output: "server",
   adapter: vercel(),
   vite: {
     ssr: {
       // Example: Force a broken package to skip SSR processing, if needed
-      noExternal: ['react-icons'],
+      noExternal: ['react-icons']
     }
   }
 });
---
+++ src/components/Navbar/index.svelte
@@ -0,0 +1,42 @@
+<script>
+  // import ThemeSelector from "./ThemeSelector"
+  const navbars = [
+    {
+      label: "Tim Jinna",
+      href: "/#",
+    },
+    {
+      label: "Exp",
+      href: "/#exp",
+    },
+    {
+      label: "Tech",
+      href: "/tech",
+    },
+    {
+      label: "Skills",
+      href: "#skills",
+    },
+  ]
+  let current = 0
+  // const { pathname } = Astro.url
+</script>
+
+<nav class="flex justify-center fixed w-full">
+  <div class="flex gap-2 bg-white rounded-3xl m-4">
+    {#each navbars as i, idx}
+      <button
+        class={(idx === current
+          ? "text-black"
+          : "text-gray-300 hover:text-gray-800") +
+          " rounded-full p-4 text-sm font-medium"}
+        on:click={() => {
+          current = idx
+        }}
+      >
+        <a href={i.href}>{i.label}</a>
+      </button>
+    {/each}
+    <!-- <ThemeSelector client:load /> -->
+  </div>
+</nav>
