e490e462361b0b18bad6861a579a69385d285aaf
cjtim <47479090+cjtim@users.noreply.github.com>
2023-10-03 15:16:39 -0400

    update styling and stories

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,8 +4,32 @@ module.exports = {
   theme: {
     fontFamily: {
       qsan: ['Quicksand', 'sans-serif']
-    }
+    },
+    extend: {
+      keyframes: {
+        typing: {
+          "0%": {
+            width: "0%",
+            visibility: "hidden"
+          },
+          "100%": {
+            width: "100%"
+          }
+        },
+        blink: {
+          "50%": {
+            borderColor: "transparent"
+          },
+          "100%": {
+            borderColor: "white"
+          }
+        }
+      },
+      animation: {
+        typing: "typing 2s steps(20) 1 alternate, blink .7s infinite"
+      }
+    },
   },
   darkMode: "class",
-  plugins: [],
+    plugins: [],
 }
